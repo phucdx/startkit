5df7f79fdd6e786b5723ba95fd5fa843a826c72b
phucdx <dang.xuan.phuc@vareal.vn>
2023-06-29 09:20:57 +0700

    Add font awesome

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,4 +8,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.4.0/js/all.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
